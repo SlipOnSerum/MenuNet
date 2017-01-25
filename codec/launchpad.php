514ecbe9b8a23221797b3d202cbfd9fa5a91c5de
SlipSerum <appsbygamer@yahoo.co.uk>
2017-01-24 21:09:12 -0800

    Create launchpad.php

---
+++ codec/launchpad.php
@@ -0,0 +1,20 @@
+<?php>
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <link rel="stylesheet" type="text/css" href="style.css" />
+  <title> MenuNet </title>
+</head>
+<body>
+  <h2> MenuNet </h2>
+  <br>
+  <br>
+  <br>
+  <a href="//launchpad.net.gvsg.cf/redirect/013/c/takeoff1.html" title="Access the Launchpad"><h3> Access Learning Materials </h3></a>
+  <a href="sites.html" title="Access Sites"><h3> Access Sites </h3></a>
+  <a href="//launchpad.net.gvsg.cf/cookie/013/c/customize.html" title="Customize MenuNet"><h3> Customize MenuNet </h3></a>
+  <br>
+  <h5> (C) jHeight Public Distribution 2017 - An Open-Source Project Deserves to Be Kept Open-Source
+</body>
+</html>
